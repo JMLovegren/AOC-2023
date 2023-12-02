993615be12e29d88367315f7bff0ffba48f67d3f
Jacob Lovegren <122911888+JMLovegren@users.noreply.github.com>
2023-12-02 10:42:11 -0500

    turned repeated code into function

--- Day_02/Part_1/0201.py
+++ Day_02/Part_1/0201.py
@@ -1,29 +1,26 @@
-import re
-
-#Determine which games would have been possible if the bag had been loaded with 
-# only 12 red cubes, 13 green cubes, and 14 blue cubes. What is the sum of the 
-# IDs of those games?
-
 MAXRED = 12
 MAXGREEN = 13
 MAXBLUE = 14
 
-with open('input.txt') as f:
-    accum = 0
-    ctr = 0
-    for line in f:
-        ctr += 1
-        pattern = r'(\d+)\s+red'
-        redNums = re.findall(pattern, line)
-        redNums = [int(match) for match in redNums]
+import re
 
-        pattern = r'(\d+)\s+green'
-        greenNums = re.findall(pattern, line)
-        greenNums = [int(match) for match in greenNums]
+def solve():
+    with open('input.txt') as f:
+        accum = 0
+        ctr = 0
+        for line in f:
+            ctr += 1
+            maxRed = findMax(line, 'red')
+            maxGreen = findMax(line, 'green')
+            maxBlue = findMax(line, 'blue')
+            if maxRed <= MAXRED and maxGreen <= MAXGREEN and maxBlue <=MAXBLUE:
+                accum += ctr
+        print (accum)
 
-        pattern = r'(\d+)\s+blue'
-        blueNums = re.findall(pattern, line)
-        blueNums = [int(match) for match in blueNums]
-        if max(redNums) <= MAXRED and max(greenNums) <= MAXGREEN and max(blueNums) <= MAXBLUE:
-            accum += ctr
-    print (accum)
+def findMax(line, color):
+    pattern = rf'(\d+)\s+{color}'
+    numStrings = re.findall(pattern, line)
+    numList = [int(match) for match in numStrings]
+    return max(numList)
+
+solve()
--- Day_02/Part_2/0202.py
+++ Day_02/Part_2/0202.py
@@ -1,24 +1,20 @@
 import re
 
-#Determine which games would have been possible if the bag had been loaded with 
-# only 12 red cubes, 13 green cubes, and 14 blue cubes. What is the sum of the 
-# IDs of those games?
+def solve():
+    with open('input.txt') as f:
+        accum = 0
+        for line in f:
+            maxRed = findMax(line, 'red')
+            maxGreen = findMax(line, 'green')
+            maxBlue = findMax(line, 'blue')
+            accum += maxRed * maxGreen * maxBlue
 
-with open('input.txt') as f:
-    accum = 0
-    for line in f:
-        pattern = r'(\d+)\s+red'
-        redNums = re.findall(pattern, line)
-        redNums = [int(match) for match in redNums]
+        print (accum)
 
-        pattern = r'(\d+)\s+green'
-        greenNums = re.findall(pattern, line)
-        greenNums = [int(match) for match in greenNums]
+def findMax(line, color):
+    pattern = rf'(\d+)\s+{color}'
+    numStrings = re.findall(pattern, line)
+    numList = [int(match) for match in numStrings]
+    return max(numList)
 
-        pattern = r'(\d+)\s+blue'
-        blueNums = re.findall(pattern, line)
-        blueNums = [int(match) for match in blueNums]
-        
-        accum += max(redNums) * max(greenNums) * max(blueNums)
-
-    print (accum)
+solve()
