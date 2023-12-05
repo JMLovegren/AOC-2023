d55799eba321798fa49248dab15c7f9dbb35891e
Jacob Lovegren <122911888+JMLovegren@users.noreply.github.com>
2023-12-05 09:26:17 -0500

    Slight modification, but still hot garbage

--- Day_05/05.py
+++ Day_05/05.py
@@ -1,3 +1,5 @@
+import time
+
 def makeEmptySeeds():
     seeds = {
         "seeds": [],
@@ -65,10 +67,12 @@ def seedLookups(seeds):
 
 def changeSeedsRange(seeds):
     seeds["seeds"] = [(seeds["seeds"][i], seeds["seeds"][i + 1]) for i in range(0, len(seeds["seeds"]) - 1, 2)]
-
+    seeds["seeds"] = merge_ranges(seeds["seeds"])
 def seedLookups2(seeds):
-    locationNumbers = []
+    minLocationNum = float('inf')
+    print(seeds['seeds'])
     for seedPair in seeds['seeds']:
+        # right here is where I need to work on a better solution cuz this loop runs too many times
         print("starting " + str(seedPair))
         ctr = seedPair[0]
         while ctr <= seedPair[0] + seedPair[1]:
@@ -79,9 +83,33 @@ def seedLookups2(seeds):
             tempNum = translate(lightNum, seeds, "light-to-temperature map")
             humidNum = translate(tempNum, seeds, "temperature-to-humidity map")
             locNum = translate(humidNum, seeds, "humidity-to-location map")
-            locationNumbers.append(locNum)
+            if locNum < minLocationNum:
+                minLocationNum = locNum
             ctr += 1
-    return (min(locationNumbers))
+            if ctr % 1000000 == 0:
+                print(ctr)
+    return minLocationNum
+
+
+def merge_ranges(pairs):
+    # Sort the pairs based on the start values
+    sorted_pairs = sorted(pairs, key=lambda x: x[0])
+
+    merged_ranges = []
+    current_range = None
+
+    for start, count in sorted_pairs:
+        if current_range is None or start > current_range[0] + current_range[1]:
+            # If no overlap or the start is after the current range, create a new range
+            current_range = (start, count)
+            merged_ranges.append(current_range)
+        else:
+            # If there is an overlap, extend the current range
+            current_range = (current_range[0], max(current_range[1], start + count - current_range[0]))
+
+    return merged_ranges
+
+
 
 def part1():
     print("Part 1")
@@ -97,7 +125,15 @@ def part2():
     changeSeedsRange(seeds)
     print(seedLookups2(seeds))
 
+startTime = time.time()
 part1()
+endTime = time.time()
+elapsed = (endTime - startTime) * 1000
+print("Part one ran in " + str(elapsed) + " ms")
 
+startTime = time.time()
 part2()
+endTime = time.time()
+elapsed = (endTime - startTime) * 1000
+print("Part two ran in " + str(elapsed) + " ms")
 
